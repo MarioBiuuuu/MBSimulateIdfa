1a9892109522403464076cf1e4f3c2872bcc54b6
adrian <adrian@gpower.co>
2020-07-29 15:36:27 +0800

    initial

---
+++ MBSimulateIdfa/MBSimulateIdfa.h
@@ -0,0 +1,27 @@
+//
+/*******************************************************************************
+    Copyright © 2019 Adrian. All rights reserved.
+
+    File name:     LGSimulateIDFA.h
+    Author:        Adrian
+
+    Project name:  LuckyGoal
+
+    Description:
+    
+
+    History:
+            2019/12/18: File created.
+
+********************************************************************************/
+    
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MBSimulateIdfa : NSObject
++ (NSString *)createSimulateIDFA;
+@end
+
+NS_ASSUME_NONNULL_END
